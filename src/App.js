455265e0e4b74d636c6f24e62f33b100b640f767
Kameshwaran Murugan <kameshwaran@crayond.co>
2024-05-08 09:20:00 +0530

    May 8th - Hooks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import './App.css';
-import { ChildComponent } from './components/ChildComponent';
-import { Greetings } from './components/Greetings';
-import { Button } from 'react-bootstrap';
 
 function App() {
 
-  const inputRef = useRef();
+  const [mangoCount, updateMangoCount] = React.useState(0);
+  const [orangeCount, updateOrangeCount] = React.useState(0);
+  const [bananaCount, updateBananaCount] = React.useState(0);
 
-  const onSubmit = () => {
-    alert(inputRef.current.value);
-    inputRef.current.style.width = "100%";
-  };
+  const [customerCount, updateCustomerCount] = React.useState(0);
+
+  const [lastUpdated, updateLastUpdate] = React.useState("");
+
+  React.useEffect(() => {
+    console.log("I will be called all the time, when a re-render happens.")
+  });
+
+  React.useEffect(() => {
+    console.log("I will be called only once, when the component rendered for the first time.")
+  }, []);
+
+  React.useEffect(() => {
+    console.log("Mango is updated");
+    updateLastUpdate("Mango");
+  }, [mangoCount]);
+
+  React.useEffect(() => {
+    console.log("Orange is updated");
+    updateLastUpdate("Orange");
+  }, [orangeCount]);
+
+  React.useEffect(() => {
+    console.log("Banana is updated");
+    updateLastUpdate("Banana");
+  }, [bananaCount]);
+
+  const getTotalStock = useCallback(() => {
+    return mangoCount + orangeCount + bananaCount
+  }, [mangoCount, bananaCount, orangeCount]);
+
+  const totalStock = useMemo(() => {
+    console.log("I am calculating...")
+    return mangoCount + bananaCount + orangeCount
+  }, [mangoCount, bananaCount, orangeCount]);
 
   return (<>
-    <input ref={inputRef} placeholder='Enter name' type='text' />
-    <button onClick={onSubmit}>Submit</button>
+    <p>No of Mangos: {mangoCount}</p>
+    <button onClick={() => updateMangoCount(mangoCount + 1)} >+</button>
+    <button onClick={() => updateMangoCount(mangoCount - 1)}>-</button>
 
-    <ChildComponent ref={inputRef} />
+    <p>No of Oranges: {orangeCount}</p>
+    <button onClick={() => updateOrangeCount(orangeCount + 1)} >+</button>
+    <button onClick={() => updateOrangeCount(orangeCount - 1)}>-</button>
 
-    <Greetings greetingPrefix={"Welcome all"} name={"Kamesh"} />
+    <p>No of Bananas: {bananaCount}</p>
+    <button onClick={() => updateBananaCount(bananaCount + 1)} >+</button>
+    <button onClick={() => updateBananaCount(bananaCount - 1)}>-</button>
 
-    <Button>My Button</Button>
+    <p>No of Customers: {customerCount}</p>
+    <button onClick={() => updateCustomerCount(customerCount + 1)} >+</button>
+    <button onClick={() => updateCustomerCount(customerCount - 1)}>-</button>
+
+    <p>Last updated: {lastUpdated}</p>
+    <p>Total Stock: {getTotalStock()}</p>
+    <p>Total Stock: {totalStock}</p>
   </>);
 }
 

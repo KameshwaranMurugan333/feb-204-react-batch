a2014c07a875a8daed943d6c92ca9d9916d3904f
Kameshwaran Murugan <kameshwaran@crayond.co>
2024-05-10 08:50:24 +0530

    Home page upated

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { AppRoutes } from "../../router/routes";
 
 export const HomeScreen = (props) => {
-    return <>HomeScreen</>
+    return <>
+        <h1>Home Screen</h1>
+        <Link to={AppRoutes.profile}>Profile</Link>
+    </>
 }
